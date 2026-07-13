563685ac7e85489ea6017f8a4ce1a756fa890b9e
Cristian VivoBook <cristianfmorac@gmail.com>
2026-07-13 14:59:53 -0500

    role sns

--- modules/observability/sns/main.tf
+++ modules/observability/sns/main.tf
@@ -3,3 +3,48 @@ resource "aws_sns_topic" "this" {
   display_name = var.display_name
   tags = var.tags
 }
+
+resource "aws_sns_topic_policy" "this" {
+  arn = aws_sns_topic.this.arn
+  policy = data.aws_iam_policy_document.this.json
+}
+
+data "aws_iam_policy_document" "this" {
+
+  statement {
+    sid    = "AllowEventBridgeToPublish"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions = [
+      "sns:Publish"
+    ]
+
+    resources = [
+      aws_sns_topic.this.arn
+    ]
+  }
+
+  statement {
+    sid    = "AllowChatbotToSubscribe"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["chatbot.amazonaws.com"]
+    }
+
+    actions = [
+      "sns:GetTopicAttributes",
+      "sns:Subscribe"
+    ]
+
+    resources = [
+      aws_sns_topic.this.arn
+    ]
+  }
+}
