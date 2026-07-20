86341b2976d4277a3e2c0042d6226e5388b64d7d
Cristian VivoBook <cristianfmorac@gmail.com>
2026-07-20 18:10:35 -0500

    variables del proyecto

--- QA/Observability/Health/locals.tf
+++ QA/Observability/Health/locals.tf
@@ -1,4 +1,27 @@
 locals {
+
+  Project = "Notify_Health"
+
+  # SNS
+  sns_display_name = "AWS Health Notifications"
+
+  # Eventbridge
+  bus_description = "Monitoreo Health de aws"
+  event_pattern = ["aws.health"]
+
+
+  # Chatbot
+  
+  ## Role chatbot
+  chatbot_role_arn = coalesce(
+    var.iam_role_arn,
+    module.chatbot_role.role_arn_chatbot
+  )
+  ## Slack
+  team_id = "T03MN099YP6"
+  channel_id = "C0BH1REFAPL"
+
+  # Tags
   default_tg = {
     Owner = "CloudTeam"
     Environment = "QA"
@@ -6,4 +29,5 @@ locals {
     ManagedBy = "Terraform"
     Application = "Observability_Health"
   }
+
 }
