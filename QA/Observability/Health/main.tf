1d9579b212ea80fa8239c6067a4851266bd38d00
Cristian VivoBook <cristianfmorac@gmail.com>
2026-07-20 18:15:03 -0500

    Validar role del chatbot

--- QA/Observability/Health/main.tf
+++ QA/Observability/Health/main.tf
@@ -1,51 +1,49 @@
 module "health_sns" {
 
   source = "../../../modules/observability/sns"
-  name = "alert-health-sns"
-  display_name = "AWS Health Notifications"
+  name = "alert-${local.Project}"
+  display_name = local.sns_display_name
   tags = merge(local.default_tg, {
-    Name = "alert-health-sns"
+    Name = "alert-${local.Project}"
     Component = "SNS"
   })
 }
 
 module "health_eventbridge" {
   source = "../../../modules/observability/eventbridge"
-  event_bus_name = "observability-qa"
-  description = "Monitoreo Health de aws"
-  rule_name = "events-rule-health"
+  event_bus_name = "observability-${local.Project}"
+  description = local.bus_description
+  rule_name = "events-rule-${local.Project}"
   event_pattern = jsonencode({
-    source = [
-      "aws.health"
-    ]
+    source = local.event_pattern
   })
 
   target_arn = module.health_sns.arn
 
   tags = merge(local.default_tg, {
-    Name = "Event_rule_Health"
+    Name = "Event_rule-${local.Project}"
     Component = "Eventbridge"
   })
 }
 
 module "chatbot_role" {
-  source = "../../../modules/observability/role/Cahtbot"
-  role_name = "observability-chatbot-role"
+  source = "../../../modules/observability/role/Chatbot"
+  role_name = "${local.Project}-chatbot-role"
   tags = merge(local.default_tg, {
-    Name = "Chatbot_role_Health"
+    Name = "Chatbot_role-${local.Project}"
     Component = "Role"
   })
 }
 
 module "slack_chatbot" {
   source = "../../../modules/observability/chatbot"
-  configuration_name = "observability-qa"
-  slack_team_id = "T03MN099YP6"
-  slack_channel_id = "C0BH1REFAPL"
-  iam_role_arn = module.chatbot_role.role_arn_chatbot
+  configuration_name = "observability-${local.Project}"
+  slack_team_id = local.team_id
+  slack_channel_id = local.channel_id
+  iam_role_arn = local.chatbot_role_arn
   sns_topic_arns = [module.health_sns.arn]
   tags = merge(local.default_tg, {
-    Name = "Chatbot_Health"
+    Name = "Chatbot-${local.Project}"
     Component = "Chatbot"
   })
 }
